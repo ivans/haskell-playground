56493c3b610e9baa58f8924b2d410caf2cf42e66
Ivan Senji <ivan.senji@gmail.com>
2015-01-19 23:33:30 +0100

    Some functions

--- src/MyFunctions.hs
+++ src/MyFunctions.hs
@@ -8,3 +8,11 @@ mul x y = x * y
 
 haqify :: [Char] -> [Char]
 haqify s = "Haq! " ++ s
+
+myDrop :: (Ord a, Num a) => a -> [a] -> [a]
+myDrop n xs = if n <= 0 || null xs
+              then xs
+              else myDrop (n - 1) (tail xs)
+
+isOdd :: Integral a => a -> Bool
+isOdd n = mod n 2 == 1
